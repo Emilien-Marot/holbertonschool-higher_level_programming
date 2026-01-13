9eb60096da38bf88bbcbdd75ec346f08470f79e2
Emilien-Marot <12394@holbertonstudents.com>
2026-01-13 10:13:52 +0000

    py v1.8.0

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord(char) in range (ord("a"), ord("z") + 1):
+            num = ord(char) + (ord("A") - ord("a"))
+        else:
+            num = ord(char)
+        print(chr(num), end="")
+    print("")
