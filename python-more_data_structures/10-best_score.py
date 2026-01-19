9f0f3424b57c6ba53248741473c9593bb42102f2
Emilien-Marot <12394@holbertonstudents.com>
2026-01-19 10:37:00 +0000

    py 6.10.1

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
     max_key = None
     for key in a_dictionary.keys():
         if max_key is None or a_dictionary[key] > a_dictionary[max_key]:
