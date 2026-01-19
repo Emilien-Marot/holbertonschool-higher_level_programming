a25daf5de3d574e3d5c4336d1d5dacd7cd5f5d61
Emilien-Marot <12394@holbertonstudents.com>
2026-01-19 06:28:19 +0000

    py 5.7.1

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     t2 = tuple_2(tuple_b)
     return (t1[0]+t2[0], t1[1]+t2[1])
 
+
 def tuple_2(my_tuple):
     if len(my_tuple) == 0:
         return (0, 0)
