bfe0a7ad5027291c0f361a9c08c1dcf1dd0f1762
Emilien-Marot <12394@holbertonstudents.com>
2026-01-19 10:35:48 +0000

    py 6.10.0

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_key = None
+    for key in a_dictionary.keys():
+        if max_key is None or a_dictionary[key] > a_dictionary[max_key]:
+            max_key = key
+    return max_key
