4d5fdfede390d7f825c3a9f7f1811c2761a2d678
Emilien-Marot <12394@holbertonstudents.com>
2026-01-20 05:43:02 +0000

    py 6.12.0

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict_rom = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    if roman_string == None or len(roman_string) == 0:
+        return None
+    i = False
+    val = 0
+    prev = None
+    for letter in roman_string:
+        if letter not in dict_rom.keys():
+            return None
+        digit = dict_rom[letter]
+        if prev != None:
+            if prev < digit:
+                val -= prev
+            else:
+                val += prev
+        prev = digit
+    val += prev
+    return val
