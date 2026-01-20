58a1db84150951a6e5a45c0f1f070502ddd08648
Emilien-Marot <12394@holbertonstudents.com>
2026-01-20 05:52:30 +0000

    py 6.12.3

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dict_rom = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
-    if roman_string == None or type(roman_string) != str or len(roman_string) == 0:
+    dict_rom = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000}
+
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     i = False
     val = 0
@@ -10,7 +18,7 @@ def roman_to_int(roman_string):
         if letter not in dict_rom.keys():
             return 0
         digit = dict_rom[letter]
-        if prev != None:
+        if prev is not None:
             if prev < digit:
                 val -= prev
             else:
