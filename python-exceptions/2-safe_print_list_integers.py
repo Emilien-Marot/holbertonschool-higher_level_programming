8e2dccbb9a1e4c8ca28b0155692303a40784ac8f
Emilien-Marot <12394@holbertonstudents.com>
2026-01-20 11:09:12 +0000

    py 7.2.1

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,8 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
     for i in range(0, x):
+        val = my_list[i]
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(val), end="")
             num += 1
         except Exception:
             continue
