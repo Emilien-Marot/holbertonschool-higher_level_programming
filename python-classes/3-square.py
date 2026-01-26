bffc95ad1a147cba56b22a025cf3ca0d72bbfed4
Emilien-Marot <12394@holbertonstudents.com>
2026-01-26 10:34:08 +0400

    py v9.3.0

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+godzilla vs king kong - ERB
+'''
+
+
+class Square:
+    '''
+    Docstring for Square 2: Electric Boogaloo
+    '''
+    __size = 0
+
+    def __init__(self, size=0):
+        '''were you expecting serious comments ?'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
