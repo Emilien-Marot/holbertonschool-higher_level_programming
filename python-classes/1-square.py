e5805eafe4af624e6d1a6b9588a533f5143581a2
Emilien-Marot <12394@holbertonstudents.com>
2026-01-26 10:14:08 +0400

    py v9.1.0

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+godzilla vs king kong - ERB
+'''
+
+
+class Square:
+    '''
+    Docstring for Square 2: Electric Boogaloo
+    '''
+
+    def __init__(self, size):
+        '''were you expecting serious comments ?'''
+        self.__size = size
