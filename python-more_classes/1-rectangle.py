0fa2172326697d4303477050fc92437731a72585
Emilien-Marot <12394@holbertonstudents.com>
2026-01-27 10:35:57 +0400

    py v10.1.0

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+doc T_T
+'''
+
+
+class Rectangle:
+    '''
+    Docstring for Rectangle
+    '''
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
