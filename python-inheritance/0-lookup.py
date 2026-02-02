058b9adecf88c75427fec745051abdf37835a2e4
Emilien-Marot <12394@holbertonstudents.com>
2026-02-02 09:31:59 +0400

    py v11.0.3

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+'''
+Docstring for python-inheritance.0-lookup
+'''
 def lookup(obj):
+    '''
+    Docstring for lookup
+    
+    :param obj: Description
+    '''
     return dir(obj)
