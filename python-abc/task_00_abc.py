33b4aaeb6ef223c973e66dd832ba230aaa82c804
Emilien-Marot <12394@holbertonstudents.com>
2026-02-02 14:47:18 +0400

    py v12.0.0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Docstring for python-abc.task_00_abc
+'''
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
