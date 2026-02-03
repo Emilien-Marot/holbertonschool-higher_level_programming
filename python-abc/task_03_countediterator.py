59360b3e4687156f1c67d0e2dd03b68021b7b235
Emilien-Marot <12394@holbertonstudents.com>
2026-02-03 09:47:10 +0400

    py v12.3.0.1

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -9,11 +9,13 @@ class CountedIterator:
     Docstring for CountedIterator
     '''
     def __init__(self, object):
+        print(object)
         self.object = iter(object)
         self.counter = 0
 
     def __next__(self):
         self.counter += 1
+        print(self.counter)
         return next(self.object)
 
     def get_count(self):
