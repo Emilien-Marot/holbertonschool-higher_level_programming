93db4288ad4a0f1e52b2c6c03d0d124686060a34
Emilien-Marot <12394@holbertonstudents.com>
2026-02-15 19:10:32 +0400

    py v13.4.0

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Docstring for python-input_output.0-read_file
+'''
+
+
+def from_json_string(my_str):
+    '''
+    Docstring for read_file
+    :param filename: Description
+    '''
+    import json
+    return json.loads(my_str)
