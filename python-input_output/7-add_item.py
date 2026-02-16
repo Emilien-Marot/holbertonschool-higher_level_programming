9b1231fa6d11bdfa6efd5574c6be7fa9f243bd5e
Emilien-Marot <12394@holbertonstudents.com>
2026-02-16 09:20:57 +0400

    py v13.7.1

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     list_json = load_from_json_file("add_item.json")
-except:
+except Exception:
     list_json = []
-for i in range(1,len(sys.argv)):
+for i in range(1, len(sys.argv)):
     list_json.append(sys.argv[i])
 save_to_json_file(list_json, "add_item.json")
