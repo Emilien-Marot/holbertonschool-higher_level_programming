900e2a38c5244da72065da7fb3f016cf7ce1bbb4
Emilien-Marot <12394@holbertonstudents.com>
2026-03-02 14:18:03 +0400

    py v16.8.0

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE first_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO first_table(`id`, `name`, `score`) VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
