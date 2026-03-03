b79d2f82c8c003af72e7c031cf3ee1647e8bd855
Emilien-Marot <12394@holbertonstudents.com>
2026-03-03 10:35:55 +0000

    py v17.13.0.1

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- comment
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_show_genres RIGHT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id ORDER BY number_of_shows DESC;
+SELECT tv_genres.name, COUNT(*) AS number_of_shows FROM tv_show_genres RIGHT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY tv_genres.id ORDER BY number_of_shows DESC;
