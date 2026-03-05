c1a550fde1533b94296b695e1e62824d55d857d0
Emilien-Marot <12394@holbertonstudents.com>
2026-03-05 14:16:53 +0400

    py v18.6.0

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a module-level docstring.
+It describes the purpose of the entire script/module.
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
