689120ee51c5dba4fd16273b51fa9b7842eda545
Emilien-Marot <12394@holbertonstudents.com>
2026-03-24 09:02:52 +0400

    js 0.7.1

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const args = process.argv;
+let num = NaN;
 if (args[2] !== undefined) {
-  const num = parseInt(args[2]);
-  if (!isNaN(num)) {
-    for (let i = 0; i < num; i++) {
-      console.log('C is fun')
-    }
-  } else {
-    console.log('Missing number of occurrences');
+  num = parseInt(args[2]);
+}
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun')
   }
 } else {
   console.log('Missing number of occurrences');
