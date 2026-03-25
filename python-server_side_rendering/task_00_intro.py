26e986ccd4ce17fef801b9eb3c55e5dadfd1f60d
Emilien-Marot <12394@holbertonstudents.com>
2026-03-25 15:17:24 +0400

    py 16.0.0.1

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,11 +1,19 @@
+import os
+
+
 def generate_invitations(template: str, attendees):
     if len(template) == 0:
+        print("Template is empty, no output files generated.")
         return
     if len(attendees) == 0:
+        print("No data provided, no output files generated.")
         return
-    for i in range(0, len(attendees)):
-        attendee = attendees[i]
-        with open(f'output_{i + 1}.txt', 'w') as file:
+    id_max = 1
+    while os.path.exists(f"output_{id_max}.txt"):
+        id_max += 1
+    for id in range(0, len(attendees)):
+        attendee = attendees[id]
+        with open(f'output_{id_max + id}.txt', 'w') as file:
             string = template.format(
                 name=attendee.get('name', "N/A"),
                 event_title=attendee.get('event_title', "N/A"),
