555e2eec7c4d996d22e005fd73222f7836d6e25f
Emilien-Marot <12394@holbertonstudents.com>
2026-03-31 14:54:33 +0400

    py 16.4.0

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from json import load, dumps
+from csv import DictReader
+from sqlite3 import connect, PARSE_COLNAMES
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open("items.json", "r", encoding="utf-8") as file:
+        items = load(file).get("items", [])
+    return render_template('items.html', items=items)
+
+@app.route('/products')
+def products():
+    type = request.args.get("source", "")
+    id = request.args.get("id")
+    if type == "json":
+        with open("products.json", "r", encoding="utf-8") as file:
+            items = load(file)
+    elif type == "csv":
+        with open("products.csv", "r", encoding="utf-8") as file:
+            reader = DictReader(file)
+            items = [a for a in reader]
+    elif type == "sql":
+        cursor = connect("products.db").cursor()
+        query = cursor.execute("SELECT * FROM Products")
+        colname = [ d[0] for d in query.description ]
+        items = [ dict(zip(colname, r)) for r in query.fetchall() ]
+    else:
+        return render_template('product_display.html', err="Wrong source")
+    if id is None:
+        return render_template('product_display.html', items=items)
+    item = next((sub for sub in items if str(sub['id']) == str(id)), None)
+    if item is not None:
+        return render_template('product_display.html', items=[item])
+    return render_template('product_display.html', err="Product not found")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
